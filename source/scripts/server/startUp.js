822c2b6569f7868bbb763609cf822fb687b04a54
Mehow <Kaiser.mehow@gmail.com>
2016-02-06 19:51:31 +0000

    dev env and basic server working

--- source/scripts/server/startUp.js
+++ source/scripts/server/startUp.js
@@ -1,1 +1,18 @@
-console.log("started up");
+import express from 'express';
+import http from 'http';
+import path from 'path';
+
+let app = express();
+let httpServer = http.Server(app);
+
+var serverDir = '../..';
+
+app.use(express.static(path.join(__dirname, serverDir + '/public')));
+
+httpServer.listen(80, function () {
+
+    var host = httpServer.address().address;
+    var port = httpServer.address().port;
+
+    console.log('App listening at http://%s:%s', host, port);
+});
